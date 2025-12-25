27a652ae19fd380b9c2fc6e1c93c894c4ea894e5
JAG7070 <pej@crawlsoft.com>
2025-12-25 23:14:30 +0100

    Made an auth controller with google account authorization and a signal controller

---
+++ SignalApi/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using Google.Apis.Auth;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SignalApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("google")]
+        public async Task<IActionResult> ConfirmGoogleIdentity(string idToken)
+        {
+            try
+            {
+                var Payload = await GoogleJsonWebSignature.ValidateAsync(idToken);
+                var result = Ok(new { Message = "Identity confirmed" });
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return Unauthorized(new { Message = "Invalid ID token", Error = ex.Message });
+            }
+        }
+    }
+}
---
+++ SignalApi/Controllers/SignalsController.cs
@@ -0,0 +1,18 @@
+﻿using Google.Apis.Auth;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SignalApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    //[Authorize]
+    public class SignalsController : ControllerBase
+    {
+        [HttpPost("wav")]
+        public IActionResult AddWavs(IEnumerable<Signal> signals)
+        {
+            return Ok(new { Message = "Wavs received", Count = signals.Count() });
+        }
+    }
+}
